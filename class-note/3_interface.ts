7a0a381be7050185fd8c9310130c1b01cb474657
kwon <tnsdyd10@naver.com>
2022-06-20 16:46:31 +0900

    learning Interface

--- class-note/3_interface.ts
+++ class-note/3_interface.ts
@@ -21,3 +21,39 @@ const capt2 = {
     age: 30
 } // o
 getUser(capt2);
+
+// 함수의 스펙(구조)에 인터페이스를 활용
+
+interface sumFunction{
+    (a:number, b:number): number;
+}
+let sum : sumFunction;
+sum = function(a:number, b:number): number{
+    return a + b;
+}
+
+// 인덱싱
+interface StringArray{
+    [index:number]: string;
+}
+
+let arr: StringArray=['a','b','c'];
+// arr[0] = 10 // x
+arr[0] = "hi" // o
+
+// 딕셔너리 패턴
+interface StringRegexDictionary{
+    [key:string]: RegExp // RegExp : 정규표현식 생성자
+}
+
+var obj:StringRegexDictionary={
+    //sth:/abc/,
+    cssFile: /\.css$/,
+    jsFile: /\.js$/
+}
+
+Object.keys(obj).forEach(element => {
+    // element: string으로 자동으로 타입 정의 미리 해줌 (추론)
+});
+
+// obj['cssFile']='a' // x
